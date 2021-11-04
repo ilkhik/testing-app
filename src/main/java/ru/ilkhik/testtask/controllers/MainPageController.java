391704fb76e5971222fb59b37c9d22fee3da3ab0
hikmatov708 <hikmatov708@gmail.com>
2021-11-04 11:13:56 +0300

    some changes

--- src/main/java/ru/ilkhik/testtask/controllers/MainPageController.java
+++ src/main/java/ru/ilkhik/testtask/controllers/MainPageController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import ru.ilkhik.testtask.config.details.UserDetailsImpl;
 import ru.ilkhik.testtask.models.User;
 
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class MainPageController {
     @RequestMapping("/")
     public String getMainPage(Authentication authentication, Model model) {
         User user = ((UserDetailsImpl) authentication.getPrincipal()).getUser();
+        model.addAttribute("signupDate", user.getSignupDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
         model.addAttribute("user", user);
         return "main";
     }
--- src/main/java/ru/ilkhik/testtask/transfer/UserDto.java
+++ src/main/java/ru/ilkhik/testtask/transfer/UserDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import ru.ilkhik.testtask.models.Role;
 import ru.ilkhik.testtask.models.User;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,12 +15,14 @@ public class UserDto {
     private Integer id;
     private String login;
     private Role role;
+    private LocalDate signupDate;
 
     public static UserDto from(User user) {
         return builder()
                 .id(user.getId())
                 .login(user.getLogin())
                 .role(user.getRole())
+                .signupDate(user.getSignupDate())
                 .build();
     }
 
